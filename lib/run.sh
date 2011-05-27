49e693bdf35ec4e062d873aa76224ff2a8b3d0fb
Philipp Comans <github@volton.otherinbox.com>
2011-05-27 23:37:22 +0200

    it finally works

--- lib/run.sh
+++ lib/run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-
 ruby_version=$(ruby -v)
 dir=$(dirname $0)
 if [[ $ruby_version == jruby* ]];
 then
-    ruby "-J-Xmx2g" "${dir}/../lib/annotate-gff.rb" $*
+    ruby "-J-Xmx2g" "${dir}/../lib/annotate-gff.rb" "$@"
 else
-    ruby "${dir}/../lib/annotate-gff.rb" $*
+    ruby "${dir}/../lib/annotate-gff.rb" "$@"
 fi
